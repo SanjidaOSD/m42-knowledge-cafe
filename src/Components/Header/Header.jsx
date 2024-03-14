49c63d5f772a9167ff6fb47f4d304c5c41ebf7aa
unknown <sanjidaema81@gmail.com>
2024-03-14 22:17:51 +0600

    created knowledge fake data

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <div>
+
+            <h1 className='text-center text-6xl font-bold'>Knowledge Cafe</h1>
+        </div>
+
+    );
+};
+
+export default Header;
